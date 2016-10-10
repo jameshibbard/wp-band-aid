13e1739616fe79b0fb069384d1c7dab8e9a13c2f
James Hibbard <james.hibbard@sitepoint.com>
2016-10-10 10:28:36 +0200

    Adds 'Open in Parsely' and 'Get total hits' to channel page

--- scripts/main-site/channels.js
+++ scripts/main-site/channels.js
@@ -16,7 +16,12 @@ const MainChannels = (function() {
   const $articlesContainer = $(".sp🚧 .l-pv4");
   const $channelBanner = $(".ChannelBanner");
   const $channelBannerInner = $(".ChannelBanner div div").first();
-  let $toggleElementsLink;
+  const $articlePannels = $(".HomePanel_content");
+  const $infiniteScrollTrigger = $("#Latest_infiniteScrollTrigger").prev();
+
+  let API_KEY, SECRET_TOKEN;
+
+  let $toggleElementsLink, $linksTemplate;
 
   function toggleElements(){
     $.each(elems, function(key, $elem) {
@@ -80,9 +85,93 @@ const MainChannels = (function() {
     $channelBannerInner.append($toggleElementsLink);
   }
 
+  function openInParsely(postUrl){
+    const parselyUrl = 'https://dash.parsely.com/sitepoint.com/find?url=' +
+                        encodeURIComponent(postUrl);
+    window.open(parselyUrl,'_blank');
+  }
+
+  function buildApiUrl(postUrl, days){
+    const apiUrl = "https://api.parsely.com/v2/analytics/post/detail?url=" +
+                   encodeURIComponent(postUrl) +
+                   "&apikey=" + API_KEY +
+                   "&secret=" + SECRET_TOKEN +
+                   "&days=" + days;
+    return apiUrl;
+  }
+
+  function getTotalHits(postUrl, $link){
+    $.get(postUrl, function(data){
+      const publishedOn = $(data).filter("meta[property='article:published_time']").attr('content');
+      const days = moment().diff(moment(publishedOn), "days") + 1;
+
+      $.getJSON(buildApiUrl(postUrl, days), function(json){
+        const hits = json.data[0]._hits
+        $link.text(`${hits} views in ${days} days`).fadeIn();
+      });
+    });
+  }
+
+  function attachEventHandlers(){
+    $(document).on("click", ".parsely-link-block", function(e){
+      e.preventDefault();
+
+      const $link = $(e.target);
+      const postUrl = $link.closest(".HomePanel").attr("href");
+
+      if(e.target.className === "open-parsely"){
+        openInParsely(postUrl);
+      } else if(e.target.className === "get-total-views"){
+        $link.text("fetching ...").fadeIn(1000);
+        getTotalHits(postUrl, $link);
+      }
+    });
+  }
+
+  function addParselyLinks(){
+    getTemplate("parsely-links.html")
+    .then(function(){
+      $(".HomePanel_content").each(function(){
+        var hasParselyLinks = $(this).find("div.parsely-link-block").length;
+        if(!hasParselyLinks){
+          $(this).append($linksTemplate);
+        }
+      });
+    });
+  }
+
+  function mutationObserverCallback(mutations) {
+    var mutationRecord = mutations[0];
+    if (mutationRecord.addedNodes[0] !== undefined){
+      // New posts were loaded
+      addParselyLinks();
+    }
+  }
+
+  function addInfiniteScrollObserver(){
+    // Ensures that "Open in Parsely" links get added
+    // to dynamically loaded posts
+    //
+    const observer = new MutationObserver(function(mutations) {
+      mutationObserverCallback(mutations);
+    });
+
+    observer.observe(
+      $infiniteScrollTrigger[0],
+      { childList: true }
+    );
+  }
+
   function init(){
-    addToggleLink();
-    setInitialLinkState();
+    getTemplate("parsely-links.html")
+    .then(function(html){
+      $linksTemplate = html;
+      $articlePannels.append($linksTemplate);
+      addToggleLink();
+      setInitialLinkState();
+      addInfiniteScrollObserver();
+      attachEventHandlers();
+    });
   }
 
   return {
